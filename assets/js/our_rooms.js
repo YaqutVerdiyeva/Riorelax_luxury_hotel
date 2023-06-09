109120aa3421e85e4307da2ce06bc395b280dcde
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2023-06-10 00:45:55 +0400

    room cards are dynamic in Our Rooms page

--- assets/js/our_rooms.js
+++ assets/js/our_rooms.js
@@ -1,6 +1,71 @@
 let modal = document.getElementById("myModal");
 let btn = document.getElementById("myBtn");
 let span = document.getElementsByClassName("close")[0];
+let BASE_URL = `http://localhost:3000/rooms`;
+
+let roomCard = document.querySelector(".rooms");
+let searchInput = document.querySelector(".search");
+let sortBtn = document.querySelector(".sort");
+let loadMore = document.querySelector(".loadmore");
+let maxlength = 3;
+let sorted = "asc";
+let filteredArr = [];
+let copyArr = [];
+
+async function getAllCards() {
+  let res = await axios(BASE_URL);
+  let data = res.data;
+  copyArr = data;
+  roomCard.innerHTML = "";
+  filteredArr = filteredArr.length || searchInput.value ? filteredArr : data;
+  filteredArr.slice(0, maxlength).forEach((el) => {
+    roomCard.innerHTML += `
+    <div class="col-lg-4 col-md-6 room-card">
+    <div class="room">
+      <div class="card">
+        <img src="${el.photo}" alt="" />
+        <div class="card-content">
+          <h2>${el.title}</h2>
+          <p>${el.price}$</p>
+          <a href="#book-room" class="button"> Book now$ </a>
+        </div>
+      </div>
+    </div>
+  </div>
+        `;
+  });
+}
+getAllCards();
+
+searchInput.addEventListener("input", function (e) {
+  filteredArr = copyArr;
+  filteredArr = filteredArr.filter((el) =>
+    el.title.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
+  );
+  getAllCards();
+});
+
+sortBtn.addEventListener("click", function () {
+  if (sorted === "asc") {
+    filteredArr.sort((a, b) => a.price - b.price);
+    sorted = "dsc";
+    sortBtn.innerHTML = "SORT Low to High";
+  } else if (sorted === "dsc") {
+    filteredArr.sort((a, b) => b.price - a.price);
+    sorted = "def";
+    sortBtn.innerHTML = "SORT High to Low";
+  } else {
+    filteredArr = copyArr;
+    sorted = "asc";
+    sortBtn.innerHTML = "SORT";
+  }
+  getAllCards();
+});
+
+loadMore.addEventListener("click", function () {
+  maxlength = maxlength + 3;
+  getAllCards(filteredArr);
+});
 
 //Modal video
 btn.onclick = function () {
@@ -16,4 +81,3 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
-
