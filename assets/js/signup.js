f7d700fc3acb151de86880659376bb35aaaf4b0f
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2023-06-13 02:12:11 +0400

    add validation in Sign Up page

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -8,10 +8,43 @@ let signUpBtn = document.querySelector(".get-submit");
 
 //post
 signUpBtn.addEventListener("click", function () {
-  axios.post(`${BASE_URL}`, {
-    firstname: firstName.value,
-    lastname: lastName.value,
-    email: email.value,
-    password: password.value,
-  });
+  if (firstName.value && lastName.value && email.value && password.value) {
+    axios.post(`${BASE_URL}`, {
+      firstname: firstName.value,
+      lastname: lastName.value,
+      email: email.value,
+      password: password.value,
+    });
+  }
+  if (
+    email.value == "" ||
+    lastName.value == "" ||
+    firstName.value == "" ||
+    password.value
+  ) {
+    lastName.style.border = "2px solid brown";
+    firstName.style.border = "2px solid brown";
+    email.style.border = "2px solid brown";
+    password.style.border = "2px solid brown";
+    document.querySelector(".validationname").style.display = "block";
+    document.querySelector(".validationlastname").style.display = "block";
+    document.querySelector(".validationemail").style.display = "block";
+    document.querySelector(".validationpassword").style.display = "block";
+  }
+});
+firstName.addEventListener("input", function () {
+  firstName.style.border = "";
+  document.querySelector(".validationname").style.display = "none";
+});
+lastName.addEventListener("input", function () {
+  lastName.style.border = "";
+  document.querySelector(".validationlastname").style.display = "none";
+});
+email.addEventListener("input", function () {
+  document.querySelector(".validationemail").style.display = "none";
+  email.style.border = "";
+});
+password.addEventListener("input", function () {
+  document.querySelector(".validationpassword").style.display = "none";
+  password.style.border = "";
 });
