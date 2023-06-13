a63b974fd127f840706725ea60df2edb63ce6adc
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2023-06-14 00:44:42 +0400

    add alerts in Login page

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,10 +6,29 @@ login.addEventListener("click", async function () {
   let res = await axios(BASE_URL);
   let data = await res.data;
   console.log(data);
-  if (data.find((item) => item.email == email.value && item.password == password.value)) {
+  if (
+    data.find(
+      (item) => item.email == email.value && item.password == password.value
+    )
+  ) {
     localStorage.setItem("users", JSON.stringify(true));
     window.location = `index.html`;
-  } else {
-    alert("You are not user");
   }
+  if (data.find((item) => item.email != email.value)) {
+    document.querySelector(".email-alert").style.visibility = "visible";
+    email.style.border = "1px solid brown";
+  }
+  if (data.find((item) => item.password != password.value)) {
+    document.querySelector(".password-alert").style.visibility = "visible";
+    password.style.border = "1px solid brown";
+  }
+});
+
+email.addEventListener("input", function () {
+  document.querySelector(".email-alert").style.visibility = "hidden";
+  email.style.border = "";
+});
+password.addEventListener("input", function () {
+  document.querySelector(".password-alert").style.visibility = "hidden";
+  password.style.border = "";
 });
