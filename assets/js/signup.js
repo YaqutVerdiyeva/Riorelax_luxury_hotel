84173a34368df14cccc1bb62028cd3b4cfbaa0c9
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2023-06-14 01:04:07 +0400

    add alert in Sign Up page

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -17,4 +17,36 @@ signUpBtn.addEventListener("click", function () {
     });
     window.location = "login.html";
   }
+  if (firstName.value == "") {
+    document.querySelector(".validationname").style.visibility = "visible";
+    firstName.style.border = "1px solid brown";
+  }
+  if (lastName.value == "") {
+    document.querySelector(".validationlastname").style.visibility = "visible";
+    lastName.style.border = "1px solid brown";
+  }
+  if (email.value == "") {
+    document.querySelector(".validationemail").style.visibility = "visible";
+    email.style.border = "1px solid brown";
+  }
+  if (password.value == "") {
+    document.querySelector(".validationpassword").style.visibility = "visible";
+    password.style.border = "1px solid brown";
+  }
+});
+firstName.addEventListener("input", function () {
+  document.querySelector(".validationname").style.visibility = "hidden";
+  firstName.style.border = "";
+});
+lastName.addEventListener("input", function () {
+  document.querySelector(".validationlastname").style.visibility = "hidden";
+  lastName.style.border = "";
+});
+email.addEventListener("input", function () {
+  document.querySelector(".validationemail").style.visibility = "hidden";
+  email.style.border = "";
+});
+password.addEventListener("input", function () {
+  document.querySelector(".validationpassword").style.visibility = "hidden";
+  password.style.border = "";
 });
